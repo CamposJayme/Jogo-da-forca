74e5ecf622e94a4aeb339bce16fd6db7eeb83490
Jayminho <jayme.campos17@gmail.com>
2024-11-20 17:05:51 -0300

    funções de jogo ganho, verificar existência da letra, computar jogadas, mensagens finais

--- funcoes/funcoes.js
+++ funcoes/funcoes.js
@@ -1,17 +1,50 @@
-//Função que seleciona fruta de forma aleatória:
+// 1) Função que seleciona fruta de forma aleatória:
 function selecionarFrutas(fruta) {
     const frutaSelecionada = fruta[Math.floor(Math.random(0, 1) * fruta.length)];
     return frutaSelecionada;
 }
 
-//Função responsável por ocultar as demais letras da fruta selecionada e apresentar apenas a primeira letra:
+// 2) Função responsável por ocultar as demais letras da fruta selecionada e apresentar apenas a primeira letra:
 function ocultarPalavra(fruta) {
     const frutaPrimeiraLetra = fruta[0];
     const underline = "_".repeat(fruta.length - 1);
     return frutaPrimeiraLetra + underline;
 }
 
-//Função que vai atualizar a palavra oculta caso o usuário acerte a letra:
+// 3) Função responsável por validar valor inserido (deve validar apenas um caracter entre a - z):
+function validarLetraDigitada(valorInserido) {
+    if (valorInserido.length === 1 && valorInserido.match(/[a-z]/)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// 4)
+function verificarJogoGanho(palavraOculta, fruta) {
+    if (palavraOculta === fruta) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// 5)
+function verificarLetraNaPalavraEscolhida(valorInserido, fruta) {
+    if (fruta.includes(valorInserido)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// 6)
+function atualizarJogadasRestantes(erros) {
+    return 4 - erros;
+}
+
+
+// 7) Função que vai atualizar a palavra oculta caso o usuário acerte a letra:
 function atualizarPalavraOculta(valorInserido, frutaOcultada, fruta) {
     const frutaOcultadaSplit = frutaOcultada.split("");
 
@@ -24,13 +57,22 @@ function atualizarPalavraOculta(valorInserido, frutaOcultada, fruta) {
     return frutaOcultadaSplit.join('')
 }
 
-//Função responsável por validar valor inserido (deve validar apenas um caracter entre a - z):
-function validarLetraDigitada(valorInserido) {
-    if (valorInserido.length === 1 && valorInserido.match(/[a-z]/)) {
-        return true
+// 8)
+function exibirMensagemFimDeJogo(statusJogo) {
+    if (statusJogo === 'venceu') {
+        console.log("\n--------------VOCÊ VENCEU!!-------------");
     } else {
-        return false
+        console.log("\n--------------VOCÊ PERDEU!!-------------");
     }
 }
 
-export { selecionarFrutas, ocultarPalavra, atualizarPalavraOculta, validarLetraDigitada };
+export { 
+    selecionarFrutas, 
+    ocultarPalavra, 
+    validarLetraDigitada, 
+    verificarJogoGanho,
+    verificarLetraNaPalavraEscolhida,
+    atualizarJogadasRestantes,
+    atualizarPalavraOculta, 
+    exibirMensagemFimDeJogo
+};
